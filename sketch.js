4ca66c6f10c37a73567a827cbcd6aecb65038da0
MRodriguezV1 <160347114+MRodriguezV1@users.noreply.github.com>
2024-02-17 14:23:25 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 500);
+  
+}
+
+function draw() {
+  background('#51D6E9');
+  triangle (100, 350, 500, 350, 300, 50)
+  triangle (-50, 350, 300, 350, 80, 70)
+  triangle (300, 350, 650, 350, 500, 70)
+  ellipse(300, 410, 1000, 300);
+
+}
